aafe2bad51909f378b60d96c4706da585976c89a
Florin B <badeaflorien@gmail.com>
2025-08-30 09:08:01 +0300

    adding a unit test

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+import json
+from app import app
+
+def test_healthcheck():
+    client = app.test_client()
+    response = client.get('/healthcheck')
+
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data['status'] == 'ok'
+    assert 'version' in data
+    print("/healthcheck passed ")
+
+if __name__ == "__main__":
+    test_healthcheck()
